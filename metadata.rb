2dc3113e24089076f41571205d09002feab7faad
Kirt Fitzpatrick <kirt.fitzpatrick@autodesk.com>
2014-08-24 10:21:43 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'poc_cookbook_bar'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures poc_cookbook_bar'
+long_description 'Installs/Configures poc_cookbook_bar'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: poc_cookbook_bar
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
